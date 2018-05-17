ab378d219bc536b3d75906ccfb0e1ffb65b10d37
Anton Lenok <dzx912@gmail.com>
2018-05-17 23:37:10 +0300

    Исправил баг, связанный с использованием двумя серверами одного порта

--- src/main/java/org/training/chat/verticle/RestStaticVerticle.java
+++ src/main/java/org/training/chat/verticle/RestStaticVerticle.java
@@ -25,6 +25,6 @@ public class RestStaticVerticle extends AbstractVerticle {
 
         httpServer.requestHandler(httpRouter::accept);
 
-        httpServer.listen(8080);
+        httpServer.listen(8081);
     }
 }
