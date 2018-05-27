e7fee4720c60e33b86dafc36824344d309739055
Anton Lenok <dzx912@gmail.com>
2018-05-27 14:42:21 +0300

    Логирование запуска приложения

--- src/main/java/org/training/chat/MainApp.java
+++ src/main/java/org/training/chat/MainApp.java
@@ -1,13 +1,18 @@
 package org.training.chat;
 
 import io.vertx.core.Vertx;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.training.chat.verticle.RestStaticVerticle;
 import org.training.chat.verticle.RouterVerticle;
 import org.training.chat.verticle.WsServerVerticle;
 
 public class MainApp {
 
+    private final static Logger logger = LogManager.getLogger(MainApp.class);
+
     public static void main(String args[]) {
+        logger.info("Start App");
         Vertx vertx = Vertx.vertx();
 
         vertx.deployVerticle(new WsServerVerticle());
