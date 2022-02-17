a66cee86d3ec1741d570591fe49a8029d91ededb
Anton Lenok <Lenok.A.Iv@sberbank.ru>
2022-02-17 18:49:53 +0300

    Перевел ResponseMessage и ResponseAcknowledge на record

--- src/main/java/org/training/chat/data/ResponseAcknowledge.java
+++ src/main/java/org/training/chat/data/ResponseAcknowledge.java
@@ -5,19 +5,5 @@ import io.vertx.core.json.JsonObject;
 /**
  * POJO объект для ответа о выполнении метода
  */
-public class ResponseAcknowledge extends ResponseMessage {
-    private String method;
-
-    public ResponseAcknowledge(String type, JsonObject content, String method) {
-        super(type, content);
-        this.method = method;
-    }
-
-    public String getMethod() {
-        return method;
-    }
-
-    public void setMethod(String method) {
-        this.method = method;
-    }
+public record ResponseAcknowledge(String type, JsonObject content, String method) {
 }
--- src/main/java/org/training/chat/data/ResponseMessage.java
+++ src/main/java/org/training/chat/data/ResponseMessage.java
@@ -5,31 +5,5 @@ import io.vertx.core.json.JsonObject;
 /**
  * POJO объект для ответов клиенту
  */
-public class ResponseMessage {
-    private String type;
-    private JsonObject content;
-
-    public ResponseMessage() {
-    }
-
-    public ResponseMessage(String type, JsonObject content) {
-        this.type = type;
-        this.content = content;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public JsonObject getContent() {
-        return content;
-    }
-
-    public void setContent(JsonObject content) {
-        this.content = content;
-    }
+public record ResponseMessage(String type, JsonObject content) {
 }
